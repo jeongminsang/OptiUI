44e1d17bef8e9e60beb13b870708345a5e084431
jeongminsang <jong0364@gmail.com>
2025-04-29 18:34:26 +0900

    update css import

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,4 @@
 export * from './components/Button.js';
 export * from './components/Input.js';
-import './styles/stylexStyles.css';
 export * from './system/ThemeProvider.js';
 import './styles/globals.css';
